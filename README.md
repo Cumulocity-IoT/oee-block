b5d0791b2f4e0e5df93cc5ca99836d05c2212fa5
Harald Meyer <hame@softwareag.com>
2024-11-12 16:52:26 +0100

    Base tests for path 2 #18

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 **OEE (Overall Equipment Effectiveness)** is a metric for measuring the efficiency, effectiveness and performance of production processes, by breaking them down into the three components Availability, Performance, and Quality.
 
+This repository contains a custom block for the Analytics Builder that performs OEE calculations.
+
---
+++ tests/BaseTest_Path2/pysystest.py
@@ -0,0 +1,162 @@
+__pysys_title__   = r""" Category Base - Base tests path 2 (APT, APA, AQA  with quality from indicator) """ 
+#                        ================================================================================
+__pysys_purpose__ = r""" """ 
+	
+__pysys_created__ = "2024-11-12"
+
+from basetest.OeeBaseTest import OeeBaseTest
+from pysys.constants import *
+import basetest.OeeBaseTest
+
+class PySysTest(OeeBaseTest):
+    
+	def execute(self):
+		correlator = self.startAnalyticsBuilderCorrelator(blockSourceDir=f'{self.project.SOURCE}/blocks/')
+		modelId = self.createTestModel('apamax.analyticsbuilder.oee.Oee', 
+								 inputs={'status':'boolean', 'pieces':'float', 'pieces_ok':None ,'pieces_nok':None,'qok':'boolean'},
+								 parameters={'0:interval':60.0,'0:ica':10.0})		
+		self.sendEventStrings(correlator,
+                              self.timestamp(30),
+							  # OEE=100
+							  self.inputEvent('status', True, id=modelId),
+							  self.inputEvent('qok', True, id=modelId),
+                              self.timestamp(90),
+							  self.inputEvent('pieces', 10, id=modelId),
+                              self.timestamp(150),
+							  self.inputEvent('pieces', 10, id=modelId),
+                              self.timestamp(210),
+							  self.inputEvent('pieces', 10, id=modelId),
+                              self.timestamp(270),
+							  self.inputEvent('pieces', 10, id=modelId),
+							  # Availability=50
+							  self.timestamp(300),
+							  self.inputEvent('qok', True, id=modelId),							  
+							  self.inputEvent('status', False, id=modelId),
+							  self.timestamp(330),
+							  self.inputEvent('status', True, id=modelId),
+							  self.inputEvent('pieces', 5, id=modelId),
+							  self.timestamp(360),
+							  self.inputEvent('status', False, id=modelId),
+							  self.timestamp(390),
+							  self.inputEvent('status', True, id=modelId),
+							  self.inputEvent('pieces', 5, id=modelId),
+							  # Quality=50
+							  self.timestamp(420),
+							  self.inputEvent('qok', False, id=modelId),
+							  self.inputEvent('pieces', 5, id=modelId),
+							  self.timestamp(450),
+							  self.inputEvent('qok', True, id=modelId),							  
+							  self.inputEvent('pieces', 5, id=modelId),
+							  self.timestamp(480),
+							  self.inputEvent('qok', False, id=modelId),							  
+							  self.inputEvent('pieces', 5, id=modelId),
+							  self.timestamp(510),
+							  self.inputEvent('qok', True, id=modelId),							  
+							  self.inputEvent('pieces', 5, id=modelId),
+							  # Performance=50
+							  self.timestamp(570),
+							  self.inputEvent('qok', True, id=modelId),							  
+							  self.inputEvent('pieces', 5, id=modelId),
+							  self.timestamp(630),
+							  self.inputEvent('pieces', 5, id=modelId),
+							  # Availabilty=50, Performance=50
+							  self.timestamp(660),
+							  self.inputEvent('qok', True, id=modelId),							  
+							  self.inputEvent('status', False, id=modelId),
+							  self.timestamp(690),
+   							  self.inputEvent('qok', True, id=modelId),							  
+							  self.inputEvent('status', True, id=modelId),
+							  self.inputEvent('pieces', 2.5, id=modelId),
+							  self.timestamp(720),
+							  self.inputEvent('qok', True, id=modelId),							  
+							  self.inputEvent('status', False, id=modelId),
+							  self.timestamp(750),
+   							  self.inputEvent('qok', True, id=modelId),							  
+							  self.inputEvent('status', True, id=modelId),
+							  self.inputEvent('pieces', 2.5, id=modelId),
+							  self.timestamp(765),
+   							  self.inputEvent('qok', False, id=modelId),							  
+							  self.inputEvent('pieces', 2.5, id=modelId),							  
+							  # Availabilty=50, Quality=50
+							  self.timestamp(780),
+							  self.inputEvent('qok', True, id=modelId),							  
+							  self.inputEvent('status', False, id=modelId),
+							  self.timestamp(810),
+							  self.inputEvent('status', True, id=modelId),
+							  self.inputEvent('pieces', 2.5, id=modelId),
+  							  self.timestamp(825),
+							  self.inputEvent('qok', False, id=modelId),
+							  self.inputEvent('pieces', 2.5, id=modelId),
+							  self.timestamp(840),
+							  self.inputEvent('qok', True, id=modelId),							  
+							  self.inputEvent('status', False, id=modelId),
+							  self.timestamp(870),
+							  self.inputEvent('status', True, id=modelId),
+							  self.inputEvent('pieces', 2.5, id=modelId),
+  							  self.timestamp(885),
+							  self.inputEvent('qok', False, id=modelId),
+							  self.inputEvent('pieces', 2.5, id=modelId),
+							  # Performance=50, Quality=50							  
+							  self.timestamp(930),
+							  self.inputEvent('qok', True, id=modelId),							  
+							  self.inputEvent('pieces', 2.5, id=modelId),
+							  self.timestamp(960),
+							  self.inputEvent('qok', False, id=modelId),							  
+							  self.inputEvent('pieces', 2.5, id=modelId),
+							  self.timestamp(990),
+							  self.inputEvent('qok', True, id=modelId),							  
+							  self.inputEvent('pieces', 2.5, id=modelId),
+							  self.timestamp(1005),
+							  self.inputEvent('qok', True, id=modelId),							  
+							  self.inputEvent('pieces', 1.25, id=modelId),
+							  # Availabilty=50, Performance=50, Quality=50
+							  self.timestamp(1020),
+							  self.inputEvent('qok', False, id=modelId),							  
+							  self.inputEvent('pieces', 1.25, id=modelId),
+							  self.inputEvent('status', False, id=modelId),
+							  self.timestamp(1050),
+							  self.inputEvent('qok', True, id=modelId),
+							  self.inputEvent('status', True, id=modelId),
+							  self.inputEvent('pieces', 0.0, id=modelId),
+							  self.timestamp(1065),
+							  self.inputEvent('qok', False, id=modelId),
+							  self.inputEvent('pieces', 1.25, id=modelId),
+							  self.timestamp(1080),
+							  self.inputEvent('qok', True, id=modelId),
+							  self.inputEvent('status', False, id=modelId),
+							  self.timestamp(1110),
+							  self.inputEvent('qok', True, id=modelId),
+							  self.inputEvent('status', True, id=modelId),
+							  self.inputEvent('pieces', 1.25, id=modelId),
+							  # Quality=0
+							  self.timestamp(1170),
+							  self.inputEvent('qok', False, id=modelId),							  
+							  self.inputEvent('pieces', 10, id=modelId),
+                              self.timestamp(1230),
+							  self.inputEvent('pieces', 10, id=modelId),
+							  # Performance=0, Quality=0
+							  self.timestamp(1290),
+							  self.inputEvent('qok', False, id=modelId),							  							  
+							  self.inputEvent('pieces', 0, id=modelId),
+                              self.timestamp(1350),
+							  self.inputEvent('pieces', 0, id=modelId),
+							  # Availability=0,Performance=0, Quality=0
+							  self.inputEvent('status', False, id=modelId),
+							  self.inputEvent('qok', False, id=modelId),							  							  
+							  self.timestamp(1410),
+							  self.inputEvent('pieces', 0, id=modelId),
+                              self.timestamp(1470),
+							  self.inputEvent('pieces', 0, id=modelId),
+							  self.inputEvent('status', True, id=modelId),
+							  self.timestamp(1530),
+							  self.inputEvent('pieces', 10, id=modelId),
+                              )
+		correlator.flush()
+
+	def validate(self):
+		self.assertBlockOutput('timestamp', 	[90.0,	150.0,	210.0,	270.0,	330.0,	390.0,	450.0,	510.0,	570.0,	630.0,	690.0,	750.0,	810.0,	870.0,	930.0, 	990.0,	1050.0,	1110.0,	1170.0,	1230.0,	1290.0,	1350.0,	1410.0,	1470.0])
+		self.assertBlockOutput('oee',		 	[1.0,	1.0,	1.0,	1.0,	0.5,	0.5,	0.5,	0.5,	0.5,	0.5,	0.25,	0.25,	0.25,	0.25,	0.25,	0.25, 	0.125,	0.125,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0])
+		self.assertBlockOutput('availability', 	[1.0,	1.0,	1.0,	1.0,	0.5,	0.5,	1.0,	1.0,	1.0,	1.0,	0.5,	0.5,	0.5,	0.5,	1.0,	1.0,	0.5,	0.5,	1.0,	1.0,	1.0,	1.0,	0.0,	0.0])
+		self.assertBlockOutput('performance', 	[1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.5,	0.5,	0.5,	0.5,	1.0,	1.0,	0.5,	0.5,	0.5,	0.5,	1.0,	1.0,	0.0,	0.0,	0.0,	0.0])
+		self.assertBlockOutput('quality', 		[1.0,	1.0,	1.0,	1.0,	1.0,	1.0,	0.5,	0.5,	1.0,	1.0,	1.0,	1.0,	0.5, 	0.5,	0.5,	0.5,	0.5,	0.5,	0.0,	0.0,	0.0,	0.0,	0.0,	0.0])
+
